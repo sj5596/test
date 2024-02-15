1422e5225d0abcde3544de123b4690bc39695d11
edu0005 <edu0005@edu0003>
2024-02-15 10:38:44 +0900

    Project Create

---
+++ src/main/java/co/micol/prj/HomeController.java
@@ -0,0 +1,19 @@
+package co.micol.prj;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = "/")
+	public String home(Model model) {
+		
+		return "home/home";
+	}
+	
+}
